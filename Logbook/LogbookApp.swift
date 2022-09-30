2abc7ec27f11adb2e71dea849d1e960b2b6d8d61
Sam <sam.tawfik@icloud.com>
2022-09-29 22:03:47 -0700

    Initial Commit

---
+++ Logbook/LogbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogbookApp.swift
+//  Logbook
+//
+//  Created by bugs on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct LogbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
